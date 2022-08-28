b19e7aabaeb889b46cefb2018bab090198fc3e5d
Giwan Kim <giwankim@gmail.com>
2022-08-29 08:48:24 +0900

    start adding skeleton code for business logic

---
+++ libs/schema/createNotification.schema.js
@@ -0,0 +1,13 @@
+'use strict';
+const Joi = require('@hapi/joi');
+
+module.exports = Joi.object({
+  user_id: Joi.string().required(),
+  message: Joi.string(),
+  message_template_id: Joi.string(),
+  recipient: Joi.string(),
+  recipient_list_file: Joi.string(),
+  idempotent_key: Joi.string().required(),
+})
+  .xor('message', 'message_template_id')
+  .xor('recipient', 'recipient_list_file');
---
+++ libs/sqs-client.js
@@ -0,0 +1,1 @@
+const SQS = require('aws-sdk/clients/sqs');
